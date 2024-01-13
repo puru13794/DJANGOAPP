acb148ec2bbf813ffea446f63de14bc8bbea0f56
puru-reddy <puru13794@gmail.com>
2024-01-13 14:33:09 +0530

    requirement update

--- api/settings.py
+++ api/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Quick-start development settings - unsuitable for production
